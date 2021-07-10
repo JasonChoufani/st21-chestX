f25856bfbdb0db45af496547e1f83d6bc31fee7b
Jason <jason.choufani@hotmail.com>
2021-07-10 08:59:14 +0200

    Removed only png verification

--- python-files/img_verifier.py
+++ python-files/img_verifier.py
@@ -10,10 +10,9 @@ def verify(location):
         print(x_image)
         try:
             img = Image.open(x_image)
-            img.verify() #check if file is a valid image, png only
             img.getdata()[0] #check if file is corrupted
-        except OSError:
-            return False
+        except (IOError, OSError) as e :
+            return e
         return True
 
 if __name__ == "__main__":
